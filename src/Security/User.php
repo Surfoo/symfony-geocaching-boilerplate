bc7cc199c348a8f752ab6db48ff846738a67fc91
Surfoo <surfooo@gmail.com>
2025-12-07 22:41:34 +0100

    update Symfony 8.0

--- src/Security/User.php
+++ src/Security/User.php
@@ -161,7 +161,6 @@ class User implements UserInterface
     /**
      * @see UserInterface
      */
-    #[\Override]
     public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
